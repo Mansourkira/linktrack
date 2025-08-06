feeddfcbe8c5d3cfbf07e7c1dd7220eab347b30b
MansourKira <mansour.benmansour@esprit.tn>
2025-08-06 09:34:34 +0100

    chore: create themetoggle ui

---
+++ src/components/ui/ThemeToggle.tsx
@@ -0,0 +1,30 @@
+// src/components/ThemeToggle.tsx
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null; // avoid hydration mismatch
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      {resolvedTheme === "dark" ? (
+        <Sun className="h-5 w-5" />
+      ) : (
+        <Moon className="h-5 w-5" />
+      )}
+    </Button>
+  );
+}
