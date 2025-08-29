e7e60ad1ad0c2db8b9d179bf2c357bb74d454069
Ben Mansour Mansour <mansour.benmansour@esprit.tn>
2025-08-29 21:16:51 +0100

    chore: add supabase connection files

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,5 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
-
+import * as dotenv from "dotenv";
+dotenv.config();
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,12 +1,16 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import * as dotenv from "dotenv";
+dotenv.config();
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl!,
+    supabaseAnonKey!,
     {
       cookies: {
         getAll() {
