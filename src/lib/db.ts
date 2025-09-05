a842ab83088a5ed20dcbcffffb916742927d7bee
Ben Mansour Mansour <mansour.benmansour@esprit.tn>
2025-09-05 02:54:43 +0100

    chore: update db schema file

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from './schema';
+import * as schema from './schemas/schema';
 
 // Database connection
 const connectionString = process.env.DATABASE_URL!;
